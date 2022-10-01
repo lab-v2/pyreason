55284f9f861e24ca104f4bca2e9f7c7d1da372ae
DyumanAditya <dyuman2001@gmail.com>
2022-10-01 10:05:56 +0530

    cleanup program.py

--- mancalog/scripts/program/program.py
+++ mancalog/scripts/program/program.py
@@ -7,13 +7,12 @@ class Program:
 	specific_node_labels = []
 	specific_edge_labels = []
 
-	def __init__(self, net_diff_graph, tmax, facts, rules, ipls):
-		self._net_diff_graph = net_diff_graph
+	def __init__(self, graph, tmax, facts, rules, ipls):
+		self._graph = graph
 		self._tmax = tmax
 		self._facts = facts
 		self._rules = rules
 		self._ipls = ipls
-		self._interp = None
 
 	def diffusion(self, history):
 		# Set up available labels
@@ -22,20 +21,20 @@ class Program:
 		Interpretation.specific_node_labels = self.specific_node_labels
 		Interpretation.specific_edge_labels = self.specific_edge_labels
 
-		self._interp = Interpretation(self._net_diff_graph, self._tmax, history)
-		old_interp = Interpretation(self._net_diff_graph, self._tmax, history)
+		interp = Interpretation(self._graph, self._tmax, history)
+		old_interp = Interpretation(self._graph, self._tmax, history)
 		
-		self._interp.apply_facts(self._facts)
+		interp.apply_facts(self._facts)
 
-		old_interp.copy(self._interp)
-		self._interp.apply_rules(self._rules, self._facts)
+		old_interp.copy(interp)
+		interp.apply_rules(self._rules, self._facts)
 
 		# This while will be executed until a fixed point is reached
 		fp_op_cnt = 0
-		while not old_interp == self._interp:
+		while not old_interp == interp:
 			fp_op_cnt += self._tmax
-			old_interp.copy(self._interp)
-			self._interp.apply_rules(self._rules, self._facts)
+			old_interp.copy(interp)
+			interp.apply_rules(self._rules, self._facts)
 
 		print('Fixed Point iterations:', fp_op_cnt)
-		return self._interp		
+		return interp		
