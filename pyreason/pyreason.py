10ad128a566fc3fe5b49d4bc6076a3d2315cdd64
Dyuman <dyuman2001@gmail.com>
2023-01-26 23:27:21 +0530

    template for python library

--- pyreason/pyreason.py
+++ pyreason/pyreason.py
@@ -1,1 +1,29 @@
-print('in pyreason!')
+# This is the file that will be imported when "import pyreason" is called. All content will be run automatically
+
+def load_graph(path: str) -> None:
+    """Loads graph from GraphMl file path into program
+
+    :param path: Path for the GraphMl file
+    """
+    pass
+
+def load_labels(path: str) -> None:
+    """Load labels from YAML file path into program
+
+    :param path: Path for the YAML labels file
+    """
+    pass
+
+def load_facts(path: str) -> None:
+    """Load facts from YAML file path into program
+
+    :param path: Path for the YAML facts file
+    """
+    pass
+
+def load_rules(path: str) -> None:
+    """Load rules from YAML file path into program
+
+    :param path: Path for the YAML rules file
+    """
+    pass
