6a2edd7ae7614ab414a91bda760186de725813d8
dyumanaditya <dyuman2001@gmail.com>
2023-02-03 17:50:36 +0000

    modified setup.py

--- setup.py
+++ setup.py
@@ -11,27 +11,11 @@ long_description = (this_directory / "README.md").read_text()
 class Initialize(install):
     def run(self):
         install.run(self)
-        sys.path.append('./')
-        import pyreason as pr
-        print('Initializing PyReason caches')
-        graph_path = os.path.join('docs', 'hello-world', 'friends.graphml')
-        labels_path = os.path.join('docs', 'hello-world', 'labels.yaml')
-        facts_path = os.path.join('docs', 'hello-world', 'facts.yaml')
-        rules_path = os.path.join('docs', 'hello-world', 'rules.yaml')
-        ipl_path = os.path.join('docs', 'hello-world', 'ipl.yaml')
-
-        pr.settings.verbose = False
-        pr.load_graph(graph_path)
-        pr.load_labels(labels_path)
-        pr.load_facts(facts_path)
-        pr.load_rules(rules_path)
-        pr.load_inconsistent_predicate_list(ipl_path)
-        pr.reason(timesteps=2)
-
+        os.system('python initialize.py')
 
 setup(
     name = 'pyreason',
-    version = '0.0.6',
+    version = '0.0.7',
     author = 'Dyuman Aditya',
     author_email = 'dyuman.aditya@gmail.com',
     description = 'An explainable inference software supporting annotated, real valued, graph based and temporal logic',
