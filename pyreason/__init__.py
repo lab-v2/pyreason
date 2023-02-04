4ea81035ed6bcca81d962003cc3a77bf2605f15a
dyumanaditya <dyuman2001@gmail.com>
2023-02-04 04:29:42 +0000

    modified setup.py

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -1,6 +1,10 @@
 from pyreason.pyreason import *
 import os
 print('Imported PyReason for the first time. Initializing ... this will take a minute')
+package_path = os.path.dirname(__file__)
+print(package_path)
+cache_path = os.path.join(package_path, 'cache')
+print(cache_path)
 print(os.system('ls -a'))
-print(os.path.exists('./cache'))
-print(os.path.exists('./docs'))
+print(os.path.exists(cache_path))
+# print(os.path.exists('./docs'))
