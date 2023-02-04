67c2aec9b70617a8b0e9b2d20be78de5bec06d0e
dyumanaditya <dyuman2001@gmail.com>
2023-02-04 04:40:14 +0000

    modified setup.py

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -1,7 +1,7 @@
 from pyreason.pyreason import *
 import os
 print('Imported PyReason for the first time. Initializing ... this will take a minute')
-package_path = os.path.dirname(__file__)
+package_path = os.path.abspath(pyreason.__file__)
 print(package_path)
 cache_path = os.path.join(package_path, 'cache')
 print(cache_path)
