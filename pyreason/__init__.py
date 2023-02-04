9799b32c8a4c0603c16b3757a44aa1f9a9b925fd
dyumanaditya <dyuman2001@gmail.com>
2023-02-04 05:42:57 +0000

    added hello world to examples

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -5,11 +5,11 @@ package_path = os.path.abspath(os.path.dirname(pyreason.__file__))
 cache_path = os.path.join(package_path, 'cache')
 if not os.path.exists(cache_path):
     print('Imported PyReason for the first time. Initializing ... this will take a minute')
-    graph_path = os.path.join(package_path, 'docs', 'hello-world', 'friends.graphml')
-    labels_path = os.path.join(package_path, 'docs', 'hello-world', 'labels.yaml')
-    facts_path = os.path.join(package_path, 'docs', 'hello-world', 'facts.yaml')
-    rules_path = os.path.join(package_path, 'docs', 'hello-world', 'rules.yaml')
-    ipl_path = os.path.join(package_path, 'docs', 'hello-world', 'ipl.yaml')
+    graph_path = os.path.join(package_path, 'examples', 'hello-world', 'friends.graphml')
+    labels_path = os.path.join(package_path, 'examples', 'hello-world', 'labels.yaml')
+    facts_path = os.path.join(package_path, 'examples', 'hello-world', 'facts.yaml')
+    rules_path = os.path.join(package_path, 'examples', 'hello-world', 'rules.yaml')
+    ipl_path = os.path.join(package_path, 'examples', 'hello-world', 'ipl.yaml')
 
     settings.verbose = False
     load_graph(graph_path)
