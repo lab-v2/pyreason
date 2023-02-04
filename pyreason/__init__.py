e2b1b1cdb62b2f2481a811fb19bfa334a8af2b14
dyumanaditya <dyuman2001@gmail.com>
2023-02-04 04:20:14 +0000

    modified setup.py

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -1,5 +1,5 @@
 from pyreason.pyreason import *
-
+import os
 print('Imported PyReason for the first time. Initializing ... this will take a minute')
 print(os.system('ls -a'))
 print(os.path.exists('./cache'))
