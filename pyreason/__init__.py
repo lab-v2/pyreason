2cabad59e341ec7c81f131c7c50e6e4dc8452fa6
dyumanaditya <dyuman2001@gmail.com>
2023-02-05 04:56:55 +0000

    modified init.py

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -6,6 +6,8 @@ import os
 package_path = os.path.abspath(os.path.dirname(pyreason.__file__))
 cache_path = os.path.join(package_path, 'cache')
 os.environ['NUMBA_CACHE_DIR'] =  cache_path
+print(cache_path)
+print(package_path)
 if not os.path.exists(cache_path):
     print('Imported PyReason for the first time. Initializing ... this will take a minute')
     graph_path = os.path.join(package_path, 'examples', 'hello-world', 'friends.graphml')
