05f75749f9140a858e2161bf6bd0dd2192c84c2c
dyumanaditya <dyuman2001@gmail.com>
2023-02-05 17:00:27 +0000

    mod version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'pyreason',
-    version = '0.1.1',
+    version = '1.0.0',
     author = 'Dyuman Aditya',
     author_email = 'dyuman.aditya@gmail.com',
     description = 'An explainable inference software supporting annotated, real valued, graph based and temporal logic',
