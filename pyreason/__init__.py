e56f67228c7346afd97909cf1dca43060c643d12
dyumanaditya <dyuman2001@gmail.com>
2023-02-05 05:28:41 +0000

    changed cache location and added as a package

--- pyreason/__init__.py
+++ pyreason/__init__.py
@@ -1,16 +1,15 @@
-from pyreason.pyreason import *
-import os
-
 # Set numba environment variable
-
 package_path = os.path.abspath(os.path.dirname(__file__))
 cache_path = os.path.join(package_path, 'cache')
 os.environ['NUMBA_CACHE_DIR'] =  cache_path
+
+from pyreason.pyreason import *
+import os
+
 print(cache_path)
 print(package_path)
 if not os.path.exists(cache_path):
     os.makedirs(cache_path)
-    os.environ['NUMBA_CACHE_DIR'] =  cache_path
     print('Imported PyReason for the first time. Initializing ... this will take a minute')
     graph_path = os.path.join(package_path, 'examples', 'hello-world', 'friends.graphml')
     labels_path = os.path.join(package_path, 'examples', 'hello-world', 'labels.yaml')
