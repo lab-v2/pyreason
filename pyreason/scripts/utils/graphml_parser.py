db450a04b4bd3061cd4fb41734cf7a3c795184e5
dyumanaditya <dyuman2001@gmail.com>
2023-02-23 18:47:38 +0000

    fixed attribute bug for edge facts

--- pyreason/scripts/utils/graphml_parser.py
+++ pyreason/scripts/utils/graphml_parser.py
@@ -41,11 +41,18 @@ class GraphmlParser:
                 facts_node.append(f)
         for e in self.graph.edges:
             for key, value in self.graph.edges[e].items():
-                l = f'{key}-{value}'
+                # IF attribute is a float or int and it is less than 1, then make it a bound, else make it a label
+                if isinstance(value, (float, int)) and value<=1 and value>=0:
+                    l = str(key)
+                    l_bnd = float(value)
+                else:
+                    l = l = f'{key}-{value}'
+                    l_bnd = 1
+
                 if label.Label(l) not in specific_edge_labels.keys():
                     specific_edge_labels[label.Label(l)] = numba.typed.List.empty_list(numba.types.Tuple((numba.types.string, numba.types.string)))
                 specific_edge_labels[label.Label(l)].append((e[0], e[1]))
-                f = fact_edge.Fact('graph-attribute-fact', (e[0], e[1]), label.Label(l), interval.closed(1, 1), 0, 0, static=True)
+                f = fact_edge.Fact('graph-attribute-fact', (e[0], e[1]), label.Label(l), interval.closed(l_bnd, 1), 0, 0, static=True)
                 facts_edge.append(f)
 
         return facts_node, facts_edge, specific_node_labels, specific_edge_labels                
--- pyreason/scripts/utils/output.py
+++ pyreason/scripts/utils/output.py
@@ -6,35 +6,15 @@ class Output:
     def __init__(self, timestamp):
         self.timestamp = timestamp
 
-    def pickle_rule_trace(self, interpretation):
-        # Pickle rule trace
-        # DOES NOT WORK!!
-        with open(f'./output/{self.timestamp}_rule_trace_nodes.pkl', 'wb') as file:
-            pickle.dump(interpretation.rule_trace_node, file)
-        with open(f'./output/{self.timestamp}_rule_trace_edges.pkl', 'wb') as file:
-            pickle.dump(interpretation.rule_trace_edge, file)
-
-    def pickle_list_of_inconsistencies(self, interpretation):
-        with open(f'./output/{self.timestamp}_inconsistencies.pkl', 'wb') as file:
-            pickle.dump(interpretation.inconsistencies, file)
-
-    def pickle_interpretations(self, interpretation):
-        # DOES NOT WORK!!
-        # Warning! This could be very time and memory consuming for large graphs. Rule trace pickle is recommended
-        with open(f'./output/{self.timestamp}_interpreations_node.pkl', 'wb') as file:
-            pickle.dump(interpretation.interpretations_node, file)
-        with open(f'./output/{self.timestamp}_interpreations_edge.pkl', 'wb') as file:
-            pickle.dump(interpretation.interpretations_edge, file)
-
     def save_rule_trace(self, interpretation, folder='./'):
         # Saves the rule trace in a csv, human readable format
-        header = ['Time', 'Fixed-Point-Operation', 'Node', 'Label', 'Old Bound', 'New Bound', 'Occurred Due To']
+        header_node = ['Time', 'Fixed-Point-Operation', 'Node', 'Label', 'Old Bound', 'New Bound', 'Occurred Due To']
 
         # Nodes rule trace
         path = os.path.join(folder, f'rule_trace_nodes_{self.timestamp}.csv')
         with open(path, 'w') as f:
             data = []
-            max_j = 0
+            max_j = -1
 
             for i, r in enumerate(interpretation.rule_trace_node):
                 row = [r[0], r[1], r[2], r[3]._value, '-', r[4].to_str(), '-']
@@ -58,7 +38,42 @@ class Output:
                 data.append(row)
 
             # Add Clause-num to header
-            if interpretation.atom_trace:
+            if interpretation.atom_trace and max_j!=-1:
+                for i in range(1, max_j+2):
+                    header_node.append(f'Clause-{i}')
+
+            writer = csv.writer(f)
+            writer.writerow(header_node)
+            writer.writerows(data)
+        
+        header_edge = ['Time', 'Fixed-Point-Operation', 'Edge', 'Label', 'Old Bound', 'New Bound', 'Occurred Due To']
+
+        # Edges rule trace
+        path = os.path.join(folder, f'rule_trace_edges_{self.timestamp}.csv')
+        with open(path, 'w') as f:
+            data = []
+            max_j = -1
+
+            for i, r in enumerate(interpretation.rule_trace_edge):
+                row = [r[0], r[1], r[2], r[3]._value, '-', r[4].to_str(), '-']
+                if interpretation.atom_trace:
+                    qn, old_bnd, name = interpretation.rule_trace_edge_atoms[i]
+                    row[4] = old_bnd.to_str()
+                    # Go through all the changes in the rule trace
+                    # len(qn) = num of clauses in rule that was used
+                    row[6] = name
+
+                    # Go through each clause
+                    for j in range(len(qn)):
+                        max_j = max(j, max_j)
+                        # Node clause
+                        row.append(list(qn[j]))
+
+
+                data.append(row)
+
+            # Add Clause-num to header
+            if interpretation.atom_trace and max_j!=-1:
                 for i in range(1, max_j+2):
                     header.append(f'Clause-{i}')
 
