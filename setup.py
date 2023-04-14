9cb00d9faf14a00f7be1b556d6fd3e662eb01e8a
Dyuman Aditya <dyuman2001@gmail.com>
2023-04-14 17:44:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'pyreason',
-    version = '1.2.9',
+    version = '1.3.0',
     author = 'Dyuman Aditya',
     author_email = 'dyuman.aditya@gmail.com',
     description = 'An explainable inference software supporting annotated, real valued, graph based and temporal logic',
