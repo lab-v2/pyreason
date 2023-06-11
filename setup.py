d4042eb3cf4e97f8252b8e8b9c6c5fca4bd884a0
Dyuman <dyuman2001@gmail.com>
2023-06-11 10:39:31 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,31 +2,31 @@ from setuptools import setup, find_packages
 
 # Read the contents of README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-
 setup(
-    name = 'pyreason',
-    version = '1.6.0',
-    author = 'Dyuman Aditya',
-    author_email = 'dyuman.aditya@gmail.com',
-    description = 'An explainable inference software supporting annotated, real valued, graph based and temporal logic',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/lab-v2/pyreason',
-    license = 'BSD 3-clause',
-    project_urls = {
+    name='pyreason',
+    version='2.0.0',
+    author='Dyuman Aditya',
+    author_email='dyuman.aditya@gmail.com',
+    description='An explainable inference software supporting annotated, real valued, graph based and temporal logic',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lab-v2/pyreason',
+    license='BSD 3-clause',
+    project_urls={
         'Bug Tracker': 'https://github.com/lab-v2/pyreason/issues',
         'Repository': 'https://github.com/lab-v2/pyreason'
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent"
     ],
-    python_requires = '>3.6',
-    install_requires = [
+    python_requires='>3.6',
+    install_requires=[
         'networkx',
         'pyyaml',
         'pandas',
@@ -34,6 +34,6 @@ setup(
         'numpy',
         'memory_profiler'
     ],
-    packages = find_packages(),
+    packages=find_packages(),
     include_package_data=True
 )
