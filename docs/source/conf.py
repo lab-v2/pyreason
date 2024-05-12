88c932b4689b5f15c1ccfd289dc0851c204a6ece
Dyuman Aditya <dyuman2001@gmail.com>
2024-05-12 22:42:18 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Pyreason'
+project = 'PyReason'
 copyright = '2024, LabV2'
 author = 'LabV2'
 release = '0.1.0'
