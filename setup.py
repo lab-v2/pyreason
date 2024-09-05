761309920bd2ba483b8d1b82e0100af07c3de126
Dyuman Aditya <dyuman2001@gmail.com>
2024-09-05 07:58:04 +0200

    fixed encoding issue for pip install on github

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding='UTF-8')
 
 setup(
     name='pyreason',
