bb9fb780bf9a1d111a7d04143d9b917adaf11e6e
astewardnolan <astewardnolan@smith.edu>
2024-11-18 21:38:39 -0500

    api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ autosummary_generate = True
 #autoapi_template_dir = '_templates/autoapi'
 # Ensure autoapi_dirs points to the folder containing pyreason.py
 #autoapi_dirs = [project_root]
-autoapi_dirs = [os.path.join(project_root, 'pyreason')]  # Include only 'pyreason.pyreason'
+autoapi_dirs = [os.path.join(project_root)]  # Include only 'pyreason.pyreason'
 
 
 
