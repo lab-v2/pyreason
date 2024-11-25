c36eaa0c074734fdfaa07044085e4cb103b04a55
Dyuman Aditya <dyuman2001@gmail.com>
2024-11-25 21:30:35 +0100

    test ignore docs autoapi, remove nested structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,10 +38,10 @@ autoapi_dirs = [os.path.join(project_root)]  # Only include the pyreason directo
 #autoapi_dirs = ['../pyreason/pyreason']
 
 autoapi_root = 'pyreason'
-autoapi_ignore = ['*/scripts/*', '*/examples/*', '*/pyreason.pyreason/*']
+autoapi_ignore = ['*/scripts/*', '*/examples/*', '*/pyreason.pyreason/*', '*/__init__.py']
 
 # Ignore modules in the 'scripts' folder
-autoapi_ignore_modules = ['pyreason.scripts']
+# autoapi_ignore_modules = ['pyreason.scripts']
 
 
 autoapi_options = [
